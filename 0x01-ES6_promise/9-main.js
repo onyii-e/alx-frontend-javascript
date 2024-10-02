8cdc4373daa3b7faae7aac5674236e0f48d535e6
onyii-e <onyinyechinwaneri00@gmail.com>
2024-10-02 19:25:38 +0000

    9-try.js, 9-main.js

--- 0x01-ES6_promise/9-main.js
+++ 0x01-ES6_promise/9-main.js
@@ -3,4 +3,3 @@ import divideFunction from './8-try';
 
 console.log(guardrail(() => { return divideFunction(10, 2)}));
 console.log(guardrail(() => { return divideFunction(10, 0)}));
-
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,9 @@
-import guardrail from "./9-try";
-import divideFunction from "./8-try";
-
-console.log(
-	guardrail(() => {
-		return divideFunction(10, 2);
-	})
-);
-console.log(
-	guardrail(() => {
-		return divideFunction(10, 0);
-	})
-);
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction(), 'Guardrail was processed');
+  } catch (err) {
+    queue.push(`Error: ${err.message}`, 'Guardrail was processed');
+  }
+  return queue;
+}
