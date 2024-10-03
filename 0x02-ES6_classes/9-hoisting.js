a03f3f5d23fb5229ecb7e84bcffe8e84d821ca24
onyii-e <onyinyechinwaneri00@gmail.com>
2024-10-03 23:57:51 +0000

    9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -13,7 +13,6 @@ export class HolbertonClass {
   }
 }
 
-// Now the class is defined, you can instantiate it.
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -21,7 +20,7 @@ export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass;  // Correctly assigning holbertonClass
+    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
@@ -29,15 +28,14 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this._holbertonClass;  // Fix: use _holbertonClass
+    return this._holbertonClass;
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;  // Fix: use this, not self
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
-// Create instances of students
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
